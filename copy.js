a1a5a849e3675d5e70665834c23dfd9473dbf02f
Redshark61 <tim.vannier@gmail.com>
2021-05-11 10:12:29 +0200

    fix : ajout d'un # devant la clé de détection pour éviter les erreurs lors de l'ajout de pre et code

--- copy.js
+++ copy.js
@@ -6,21 +6,21 @@ button_transform.addEventListener("click", () => {
     code = code.replace(/\u0026/g, "&amp;");
     code = code.replace(/</g, "&lt;");
     console.log(typeof code);
-    const regex_html = new RegExp("html");
-    const regex_css = new RegExp("css");
-    const regex_js = new RegExp("js");
-    const regex_php = new RegExp("php");
+    const regex_html = new RegExp("#html");
+    const regex_css = new RegExp("#css");
+    const regex_js = new RegExp("#js");
+    const regex_php = new RegExp("#php");
     if (regex_html.test(code)) {
-        code = code.replace(/html/, "");
+        code = code.replace(/#html/, "");
         code = '<pre>\n<code class="language-html">\n' + code + "\n</code>\n</pre>";
     } else if (regex_js.test(code)) {
-        code = code.replace(/js/, "");
+        code = code.replace(/#js/, "");
         code = '<pre>\n<code class="language-javascript">\n' + code + "\n</code>\n</pre>";
     } else if (regex_php.test(code)) {
-        code = code.replace(/php/, "");
+        code = code.replace(/#php/, "");
         code = '<pre>\n<code class="language-php">\n' + code + "\n</code>\n</pre>";
     } else if (regex_css.test(code)) {
-        code = code.replace(/css/, "");
+        code = code.replace(/#css/, "");
         code = '<pre>\n<code class="language-css">\n' + code + "\n</code>\n</pre>";
     }
     document.getElementById("editeur").value = code;
