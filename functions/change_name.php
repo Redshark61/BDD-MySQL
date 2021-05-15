608cae53b36a6908d916c03fbcab3ec4e60e9309
Redshark61 <tim.vannier@gmail.com>
2021-05-15 17:24:58 +0200

    add : fonction pour changer le titre du tuto en lien lisible pour l'url

---
+++ functions/change_name.php
@@ -0,0 +1,7 @@
+<?php
+
+//On supprime tous les caractères interdits dasn les url, car le nom de fichier fera office d'url
+$forbiden_char = array('\'', '!', '.', '/', '?', ',', ';', '§', '%', '*', '$', '£', '&');
+$file_name_without_extension = str_replace($forbiden_char, '', $_POST['titre']);
+//On remplace les espaces par des _
+$file_name_extension = str_replace(' ', '_', $file_name_without_extension);
