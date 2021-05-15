96238c8f006ee47b63bdfe6772c0c3dac3dcd840
Redshark61 <tim.vannier@gmail.com>
2021-05-15 17:24:21 +0200

    fix : lorsqu'on supprime un tuto on supprime aussi le dossier et toutes les images associée

--- delete.php
+++ delete.php
@@ -9,5 +9,24 @@ $reponse->execute(array(
 ));
 
 $reponse->closeCursor();
+
+delete_files('ressources/tuto/'.$_GET['tuto']);
+
+/* 
+ * php delete function that deals with directories recursively
+ */
+function delete_files($target) {
+    if(is_dir($target)){
+        $files = glob( $target . '*', GLOB_MARK ); //GLOB_MARK adds a slash to directories returned
+
+        foreach( $files as $file ){
+            delete_files( $file );      
+        }
+
+        rmdir( $target );
+    } elseif(is_file($target)) {
+        unlink( $target );  
+    }
+}
 header('Location: affiche_liste.php', TRUE);
 exit();
