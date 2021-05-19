cebbad688ae4ab255f945637e025dc00ca149232
Redshark61 <tim.vannier@gmail.com>
2021-05-19 20:48:18 +0200

    fix : input password

--- index.php
+++ index.php
@@ -18,11 +18,11 @@ $_SESSION['connected'] = false
     <form action="traitement_inscription.php" method="POST">
         <div class="container container_mdp1">
             <label for="mdp1">Mot de passe 1</label>
-            <input type="text" name="mdp1" id="mdp1" placeholder="mdp 1">
+            <input type="password" name="mdp1" id="mdp1" placeholder="mdp 1">
         </div>
         <div class="container container_mdp2">
             <label for="mdp2">Mot de passe 2</label>
-            <input type="text" name="mdp2" id="mdp2" placeholder="mdp 2">
+            <input type="password" name="mdp2" id="mdp2" placeholder="mdp 2">
         </div>
 
         <button type="submit">Valider</button>
