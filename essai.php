bb6d196c8b524d62c73538477eee8d796ebaf1d4
T1m0th33 <tim.vannier@gmail.com>
2021-05-19 21:34:21 +0200

    fix : presque fini le passage au tout automatique

--- essai.php
+++ essai.php
@@ -1,29 +1,31 @@
 <?php
 
-$list_shortcut = array('IMG#'=>'<img src', 'H1#'=>'<h1>');
+$list_shortcut = array(
+    'IMG#'=>[
+        '<img src ="',
+        '">'
+    ],
+    'H1#'=>[
+        '<h1>',
+        '</h1>']);
 $result = "texte H1#texte# texte texte IMG#capture1.jpg# texte texte texte texte. Gardez-la ouverte avec quelque chose de pas IMG#capture2.jpg# très bien passé. Codes, H1#texte#, ordonnances, et selon toutes leurs issues, et peut-être feras-tu quelques honorables connaissances.";
 
+
 function change($result, $list_shortcut){
-    $result = preg_replace('/IMG#/','<img src ="', $result);
-
-    $result = preg_split('/(<img src =")/', $result, -1, PREG_SPLIT_NO_EMPTY | PREG_SPLIT_DELIM_CAPTURE);
-
-    // for ($i=0;$i!=count($result);$i++){
-    //     $matched_end = preg_match_all('/[(0-9A-Z){0}]#/', $result[$i], $matche_end, PREG_OFFSET_CAPTURE);
-    //     if(!$matched_end){
-            
-    //         $result[$i] = preg_replace('/#/','">', $result[$i]);
-            
-    //     }
-    // }
-    $result = implode(' ', $result);
-    $matched = preg_match_all('/<img src/', $result, $matche_end, PREG_OFFSET_CAPTURE);
-    echo '<pre>'.print_r($matche_end, true).'</pre>';
-    for ($i=0; $i!=count($matche_end); $i++){
-        $result = substr_replace($result, '">', $matche_end[0][$i][1], 1);
+    foreach($list_shortcut as $pattern){
+    echo '<pre>'.htmlspecialchars(print_r($pattern[0], true)).'</pre>';
+    $result = preg_replace('/'.$pattern[0].'/',$pattern[1], $result);
+    $match_end = preg_match_all('/'.$pattern[1].'\S|\.#/', $result, $matches, PREG_OFFSET_CAPTURE);
+    //echo '<pre>'.print_r($match_end, true).'</pre>';
+    //echo '<pre>'.htmlspecialchars(print_r($matches, true)).'</pre>';
+    for ($i=0;$i!=$match_end;$i++){
+        $result = str_split($result, $matches[0][$i][1] - 2);
+        $result[1] = preg_replace('/#/', $pattern[1],$result[1], 1);
+        $result = implode('', $result);
     }
-    
-    echo htmlspecialchars($result). '<br>';
+}
+    echo '<pre>'.print_r(htmlspecialchars($result), true).'</pre>';
+
 }
 
 change($result, $list_shortcut);
