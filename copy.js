b82a769d318c356b269de84ed9113deb8fbf8d55
Redshark61 <tim.vannier@gmail.com>
2021-05-24 16:47:08 +0200

    fix : maj en fonciton des nouveaux boutons et du système avec la sélection

--- copy.js
+++ copy.js
@@ -1,33 +1,45 @@
-const button_transform = document.querySelector(".transform");
+const button_transform = document.querySelector("#transform");
 const buttton_copy = document.querySelector(".copy");
 
 button_transform.addEventListener("click", () => {
-    var code = document.querySelector("#editeur").value;
-    code = code.replace(/\u0026/g, "&amp;");
-    code = code.replace(/</g, "&lt;");
-    console.log(typeof code);
+    var textarea = document.querySelector("#contenu");
+    s = window.getSelection();
+    oRange = s.getRangeAt(0); //get the text range
+    var beginning_text = textarea.value.substr(0, textarea.selectionStart);
+    var selected_text = window.getSelection().toString();
+    var end_text = textarea.value.substr(textarea.selectionEnd);
+    selected_text = selected_text.replace(/\u0026/g, "&amp;");
+    selected_text = selected_text.replace(/</g, "&lt;");
+    // console.log(typeof selected_text);
     const regex_html = new RegExp("#html");
     const regex_css = new RegExp("#css");
     const regex_js = new RegExp("#js");
     const regex_php = new RegExp("#php");
-    if (regex_html.test(code)) {
-        code = code.replace(/#html/, "");
-        code = '<pre>\n<code class="language-html">\n' + code + "\n</code>\n</pre>";
-    } else if (regex_js.test(code)) {
-        code = code.replace(/#js/, "");
-        code = '<pre>\n<code class="language-javascript">\n' + code + "\n</code>\n</pre>";
-    } else if (regex_php.test(code)) {
-        code = code.replace(/#php/, "");
-        code = '<pre>\n<code class="language-php">\n' + code + "\n</code>\n</pre>";
-    } else if (regex_css.test(code)) {
-        code = code.replace(/#css/, "");
-        code = '<pre>\n<code class="language-css">\n' + code + "\n</code>\n</pre>";
+    if (regex_html.test(selected_text)) {
+        selected_text = selected_text.replace(/#html/, "");
+        selected_text =
+            '</p><pre>\n<code class="language-html">\n' + selected_text + "\n</code>\n</pre><p>";
+    } else if (regex_js.test(selected_text)) {
+        selected_text = selected_text.replace(/#js/, "");
+        selected_text =
+            '</p><pre>\n<code class="language-javascript">\n' +
+            selected_text +
+            "\n</code>\n</pre><p>";
+    } else if (regex_php.test(selected_text)) {
+        selected_text = selected_text.replace(/#php/, "");
+        selected_text =
+            '</p><pre>\n<code class="language-php">\n' + selected_text + "\n</code>\n</pre><p>";
+    } else if (regex_css.test(selected_text)) {
+        selected_text = selected_text.replace(/#css/, "");
+        selected_text =
+            '</p><pre>\n<code class="language-css">\n' + selected_text + "\n</code>\n</pre><p>";
     }
-    document.getElementById("editeur").value = code;
+    result = beginning_text + selected_text + end_text;
+    textarea.value = result;
 });
 
-buttton_copy.addEventListener("click", () => {
-    var code = document.querySelector("#editeur");
-    code.select();
-    document.execCommand("copy");
-});
+// buttton_copy.addEventListener("click", () => {
+//     var selected_text = document.querySelector("#editeur");
+//     selected_text.select();
+//     document.execCommand("copy");
+// });
